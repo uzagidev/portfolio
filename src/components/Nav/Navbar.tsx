1bd78c9ad225e34eeddc6fdc8894fd507d70e1b2
danhtqbit <danhtqbit@gmail.com>
2024-06-20 21:15:05 +0000

    create RevalText

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,5 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import RevalText from "../Text/RevalText";
 
 const links = [{
   name: "About",
@@ -15,42 +16,44 @@ const links = [{
   href: "#contact"
 }];
 
+const containerVariants = {
+  open: {
+    clipPath: "circle(100% at calc(100% - 32px) 32px)",
+    transition: {
+      duration: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    clipPath: "circle(24px at calc(100% - 32px) 32px)",
+    transition: {
+      duration: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+      staggerChildren: 0.05,
+      staggerDirection: -1
+    },
+  },
+};
+
+const linkVariants = {
+  open: {
+    opacity: 1,
+    y: 0,
+  },
+  closed: {
+    opacity: 0,
+    y: 5,
+  },
+}
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const variants = {
-    open: {
-      clipPath: "circle(100% at calc(100% - 32px) 32px)",
-      transition: {
-        duration: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-        staggerChildren: 0.1,
-      },
-    },
-    closed: {
-      clipPath: "circle(24px at calc(100% - 32px) 32px)",
-      transition: {
-        duration: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-        staggerChildren: 0.05,
-        staggerDirection: -1
-      },
-    },
-  };
-  const linkVariants = {
-    open: {
-      opacity: 1,
-      y: 0,
-    },
-    closed: {
-      opacity: 0,
-      y: 5,
-    },
-  }
   return (
     <AnimatePresence>
       <div className="container-md">
@@ -63,29 +66,22 @@ const Navbar = () => {
 
           }}>
             {links.map(link => (
-              <motion.a key={link.name} href={link.href} className="text-white hover:text-black hover:bg-white px-4 py-2" variants={linkVariants}>{link.name}</motion.a>
+              <motion.a key={link.name} href={link.href} className="text-white hover:text-black hover:bg-white px-4 py-2 select-none" variants={linkVariants}>
+                  <RevalText text={link.name} speed={0.4} className="px-4 py-2"/>
+              </motion.a>
             ))}
-            {/* <a href="#about" className="text-white hover:text-black hover:bg-white px-4 py-2">About</a>
-            <a href="#exp" className="text-white hover:text-black hover:bg-white px-4 py-2">Experience</a>
-            <a href="#projects" className="text-white hover:text-black hover:bg-white px-4 py-2">Projects</a>
-            <a href="#contact" className="text-white hover:text-black hover:bg-white px-4 py-2">Contact</a> */}
           </motion.div>
           {/* side bar */}
           <motion.div className="md:hidden flex flex-col justify-center items-center fixed top-0 right-0 w-full h-screen" initial="closed" animate={isOpen ? "open" : "closed"} >
-            <motion.div className="flex flex-col justify-center items-center gap-6 bg-white h-full w-full" variants={variants} >
+            <motion.div className="flex flex-col justify-center items-center gap-6 bg-white h-full w-full" variants={containerVariants} >
               {
                 links.map(link => (
-                  <motion.a key={link.name} href={link.href} className="text-black hover:text-white hover:bg-black px-4 py-2" variants={linkVariants}>{link.name}</motion.a>
+                  <motion.a key={link.name} href={link.href} className="text-black hover:text-white hover:bg-black px-4 py-2" variants={linkVariants} onClick={() => setIsOpen(prev => !prev)}>{link.name}</motion.a>
                 ))
               }
-              {/* <a href="#about" className="text-black hover:text-white hover:bg-black px-4 py-2">About</a>
-              <a href="#exp" className="text-black hover:text-white hover:bg-black px-4 py-2">Experience</a>
-              <a href="#projects" className="text-black hover:text-white hover:bg-black px-4 py-2">Projects</a>
-              <a href="#contact" className="text-black hover:text-white hover:bg-black px-4 py-2">Contact</a> */}
             </motion.div>
             <button className="fixed top-[8px] right-[8px] p-4 border-none bg-transparent text-black rounded-full" onClick={() => setIsOpen(prev => !prev)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                {/* <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M7 12h13m-10 6h10"></path> */}
                 <motion.path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" variants={{ open: { d: "M1.68625 11.3137L13 0" }, closed: { d: "M0 1L16 1" } }}></motion.path>
                 <motion.path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.99999 7L16 7" variants={{ open: { opacity: 0 }, closed: { opacity: 1 } }}></motion.path>
                 <motion.path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" variants={{ open: { d: "M1.56233 0.0689536L12.876 11.3827" }, closed: { d: "M5.99999 13L16 13" } }}></motion.path>
